6df827901d250c9b4d02bd03d30bea199d35ae1d
SewejeJoshua <joshuaseweje2@gmail.com>
2026-09-15 14:28:20 +0100

    updated ui

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -12,8 +12,8 @@ export default function GalleryPage() {
     "/gallery/ofas-gal-2.jpg",
     "/gallery/ofas-gal-4.jpg",
     "/gallery/ofas-gal-5.jpg",
-    "/gallery/ofas-gal-6.jpg",
-    "/gallery/ofas-gal-3.jpg",
+    "/gallery/ofas-gal-6.jpg", 
+    "/gallery/ofas-gal-6.jpg", 
   ];
 
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
