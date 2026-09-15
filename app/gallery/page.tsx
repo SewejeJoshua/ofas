f604b4e84da0dfc9de07123cb3d87095edb35c15
SewejeJoshua <joshuaseweje2@gmail.com>
2026-09-15 14:49:49 +0100

    updated ui

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -8,13 +8,11 @@ import { X } from "lucide-react";
 
 export default function GalleryPage() {
   const images = [
-    
-    "/gallery/ofas-gal-9.jpg", 
     "/gallery/ofas-gal-1.jpg",
     "/gallery/ofas-gal-2.jpg",
+    "/gallery/ofas-gal-3.jpg",
     "/gallery/ofas-gal-4.jpg",
-    "/gallery/ofas-gal-5.jpg",
-    "/gallery/ofas-gal-6.jpg", 
+    "/gallery/ofas-gal-5.jpg", 
   ];
 
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
